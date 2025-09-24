4e5174ad99a1e30bc74fb650c5b852ebdc63e783
cemquo <semihfeyyaztopcu@gmail.com>
2025-09-24 10:45:32 +0200

    adaptation pour utiliser plusieurs connexions PDO

--- app/config/services.php
+++ app/config/services.php
@@ -1,27 +1,34 @@
 <?php
 declare(strict_types=1);
 
+use Psr\Container\ContainerInterface;
 use toubilib\core\application\ports\api\ServicePraticienInterface;
 use toubilib\core\application\ports\spi\repositoryInterfaces\PraticienRepositoryInterface;
 use toubilib\core\application\usecases\ServicePraticien;
 use toubilib\infra\repositories\PraticienRepository;
 
 return [
-    PDO::class => function($c) {
+
+    'pdo.prat' => function(ContainerInterface $c): PDO {
+        $db = $c->get('settings')['db']['prat'];
         return new PDO(
-            sprintf(
-                "pgsql:host=%s;port=%s;dbname=%s",
-                $c->get('db.host'),
-                $c->get('db.port'),
-                $c->get('db.name')
-            ),
-            $c->get('db.user'),
-            $c->get('db.password')
+            "{$db['driver']}:host={$db['host']};dbname={$db['dbname']}",
+            $db['user'],
+            $db['password']
+        );
+    },
+
+    'pdo.rdv' => function(ContainerInterface $c): PDO {
+        $db = $c->get('settings')['db']['pat'];
+        return new PDO(
+            "{$db['driver']}:host={$db['host']};dbname={$db['dbname']}",
+            $db['user'],
+            $db['password']
         );
     },
 
     PraticienRepositoryInterface::class => function($c) {
-        return new PraticienRepository($c->get(PDO::class));
+        return new PraticienRepository($c->get('pdo.prat'));
     },
 
     ServicePraticienInterface::class => function($c) {
--- app/config/settings.php
+++ app/config/settings.php
@@ -2,9 +2,38 @@
 declare(strict_types=1);
 
 return [
-    'db.host' => $_ENV['DB_HOST'] ?? 'toubiprati.db',
-    'db.port' => $_ENV['DB_PORT'] ?? '5432',
-    'db.name' => $_ENV['DB_NAME'] ?? 'toubiprat',
-    'db.user' => $_ENV['DB_USER'] ?? 'toubiprat',
-    'db.password' => $_ENV['DB_PASSWORD'] ?? 'toubiprat',
+
+    'settings' => [
+
+        'db' => [
+            'prat' => [
+                'driver' => $_ENV['prat.driver'],
+                'host' => $_ENV['prat.host'],
+                'dbname' => $_ENV['prat.database'],
+                'user' => $_ENV['prat.username'],
+                'password' => $_ENV['prat.password'],
+            ],
+            'pat' => [
+                'driver' => $_ENV['pat.driver'],
+                'host' => $_ENV['pat.host'],
+                'dbname' => $_ENV['pat.database'],
+                'user' => $_ENV['pat.username'],
+                'password' => $_ENV['pat.password'],
+            ],
+            'auth' => [
+                'driver' => $_ENV['auth.driver'],
+                'host' => $_ENV['auth.host'],
+                'dbname' => $_ENV['auth.database'],
+                'user' => $_ENV['auth.username'],
+                'password' => $_ENV['auth.password'],
+            ],
+            'rdv' => [
+                'driver' => $_ENV['rdv.driver'],
+                'host' => $_ENV['rdv.host'],
+                'dbname' => $_ENV['rdv.database'],
+                'user' => $_ENV['rdv.username'],
+                'password' => $_ENV['rdv.password'],
+            ],
+        ],
+    ]
 ];
